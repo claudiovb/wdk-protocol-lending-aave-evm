f6c46ce2c07bb38211b7ce0b5eac2fa5e2176809
Jonathan Dunne <37194960+jonathunne@users.noreply.github.com>
2025-10-05 03:09:03 +0100

    feat: publish workflow, migrate package name

--- index.js
+++ index.js
@@ -14,16 +14,16 @@
 
 'use strict'
 
-/** @typedef {import('@wdk/wallet').TransactionResult} TransactionResult */
+/** @typedef {import('@tetherto/wdk-wallet').TransactionResult} TransactionResult */
 
-/** @typedef {import('@wdk/wallet/protocols').BorrowOptions} BorrowOptions */
-/** @typedef {import('@wdk/wallet/protocols').BorrowResult} BorrowResult */
-/** @typedef {import('@wdk/wallet/protocols').SupplyOptions} SupplyOptions */
-/** @typedef {import('@wdk/wallet/protocols').SupplyResult} SupplyResult */
-/** @typedef {import('@wdk/wallet/protocols').WithdrawOptions} WithdrawOptions */
-/** @typedef {import('@wdk/wallet/protocols').WithdrawResult} WithdrawResult */
-/** @typedef {import('@wdk/wallet/protocols').RepayOptions} RepayOptions */
-/** @typedef {import('@wdk/wallet/protocols').RepayResult} RepayResult */
+/** @typedef {import('@tetherto/wdk-wallet/protocols').BorrowOptions} BorrowOptions */
+/** @typedef {import('@tetherto/wdk-wallet/protocols').BorrowResult} BorrowResult */
+/** @typedef {import('@tetherto/wdk-wallet/protocols').SupplyOptions} SupplyOptions */
+/** @typedef {import('@tetherto/wdk-wallet/protocols').SupplyResult} SupplyResult */
+/** @typedef {import('@tetherto/wdk-wallet/protocols').WithdrawOptions} WithdrawOptions */
+/** @typedef {import('@tetherto/wdk-wallet/protocols').WithdrawResult} WithdrawResult */
+/** @typedef {import('@tetherto/wdk-wallet/protocols').RepayOptions} RepayOptions */
+/** @typedef {import('@tetherto/wdk-wallet/protocols').RepayResult} RepayResult */
 
 /** @typedef {import('./src/aave-protocol-evm.js').AccountData} AccountData */
 
